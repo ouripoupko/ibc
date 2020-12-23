df97adfd2e0f970ef717b1eea84b8e50fb24ccf6
Ouri Poupko <ouri.poupko@gmail.com>
2020-12-23 13:14:10 +0200

    client 1 is webified

--- ibc.py
+++ ibc.py
@@ -1,7 +1,7 @@
 
 import sys
 import json
-from flask import Flask, request
+from flask import Flask, request, Response, send_from_directory
 from state import State
 from blockchain import BlockChain
 from network import Network
@@ -15,9 +15,25 @@ network = None
 me = 0
 
 
+@app.route('/favicon.ico')
+def favicon():
+  return send_from_directory(app.root_path,
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               
+@app.route('/', methods=['GET'])
+def view():  # pragma: no cover
+  f = open('ui.html','r')
+  content = f.read()
+  f.close()
+  return Response(content, mimetype="text/html")
+
+
 # Create a URL route in our application for contracts
 @app.route('/contract/', methods = ['GET','POST','PUT','DELETE'])
 def contract():
+  if request.method == 'GET':
+    print(request.get_json())
+    return "hello world"
   params = request.get_json()
   if request.method == 'PUT':
     chain.log('CREATE', params)
@@ -29,6 +45,7 @@ def contract():
       else:
         chain.log('INPUT', params)
       return state.call(params['msg'])
+    
 
 
 # Create a URL route in our application for partners
