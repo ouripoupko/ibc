c75f4097abc28bd5d7a76c233b01c2d633b7e668
ouripoupko <ouri.poupko@gmail.com>
2022-05-15 15:41:08 +0300

    community script attacks with 1,3,6 and the community grows 3.4, 1.8, 1

--- script_community.py
+++ script_community.py
@@ -1,11 +1,16 @@
 import requests
-from time import sleep
 from sseclient import SSEClient
 from threading import Thread, Semaphore
-from random import choice
+from random import choice, choices
 
 server = 'http://localhost:5001/'
-agents = [f'agent_{str(i).zfill(5)}' for i in range(200)]
+size = 3000
+agents = [f'agent_{str(i).zfill(5)}' for i in range(size)]
+deg = 10
+character = ['H' for i in range(deg+1)] + choices(
+    population=['H', 'C', 'S'],
+    weights=[0.1, 0.3, 0.6],
+    k=size-deg-1)
 contract = 'community'
 
 locks = [Semaphore(0) for s in agents]
@@ -17,8 +22,6 @@ def listener(local_index):
     messages = SSEClient(f'{server}stream/{agents[local_index]}/{contract}')
     for msg in messages:
         if msg.data == 'True':
-            # if not started or local_index == 0:
-            #     print(f'a message received from the server {local_index} {counter}')
             if started:
                 counter += 1
             locks[local_index].release()
@@ -44,38 +47,66 @@ requests.post(f'{server}ibc/app/{agents[0]}/{contract}',
               json={'pid': agents[0], 'address': server, 'code': f.read()})
 f.close()
 
-# for index in range(1, len(agents)):
-#     url = f'{server}ibc/app/{agents[index]}/{contract}'
-#     json = {'pid': agents[0], 'address': server}
-#     requests.post(url, json=json)
-#     locks[index].acquire()
-#     sleep(2+index/100)
-
 print(f'starting to send transactions')
 started = True
-deg = 10
 
 for index in range(deg+1):
     print('transact', index)
-    # transact(agents[0], 'register', {'name': agents[index]})
-    # locks[0].acquire()
-    # for second in range(index) if index < 13 else sample(range(index), 12):
-    #     transact(agents[0], 'add_friend', {'name': agents[index], 'friend': agents[second]})
-    #     locks[0].acquire()
     transact(agents[0], 'join', {'name': agents[index], 'friends': []})
     locks[0].acquire()
 
 for index in range(deg+1, len(agents)):
     community = read(agents[0], 'get_community', {})
     friends = []
-    for i in range(int(deg/2+0.1)):
-        first = choice(list(community.keys()))
-        second = None
-        while not second or second not in community:
-            second = choice(community[first]['friends'])
+    keys = list(community.keys())
+    names = [community[key]['name'] for key in keys]
+    types = [character[agents.index(name)] for name in names]
+    print(index, len(keys))
+    if character[index] == 'C':
+        if (types.count('C') + 1)/len(types) > 0.3:
+            continue
+    else:
+        if character[index] == 'H':
+            indices = [i for i, x in enumerate(types) if x != 'S']
+        else:
+            indices = [i for i, x in enumerate(types) if x != 'H']
+        keys = [keys[i] for i in indices]
+        names = [names[i] for i in indices]
+        types = [types[i] for i in indices]
+    tmp_keys = keys[:]
+    while len(friends) * 2 < deg:
+        if len(tmp_keys) < 2:
+            break
+        first = choice(tmp_keys)
+        tmp_friends = community[first]['friends'][:]
+        while tmp_friends:
+            second = choice(tmp_friends)
+            if second in tmp_keys:
+                break
+            tmp_friends.remove(second)
+        if not tmp_friends:
+            tmp_keys.remove(first)
+            continue
         friends.append([community[first]['name'], community[second]['name']])
-        del community[first]
-        del community[second]
-    print(friends)
-    transact(agents[0], 'join', {'name': agents[index], 'friends': friends})
-    locks[0].acquire()
+        if first not in keys or second not in keys:
+            print('oops!')
+        tmp = keys.index(first)
+        del keys[tmp]
+        del names[tmp]
+        del types[tmp]
+        tmp_keys.remove(first)
+        tmp = keys.index(second)
+        del keys[tmp]
+        del names[tmp]
+        del types[tmp]
+        tmp_keys.remove(second)
+    if len(friends) * 2 == deg:
+        transact(agents[0], 'join', {'name': agents[index], 'friends': friends})
+        locks[0].acquire()
+
+community = read(agents[0], 'get_community', {})
+keys = list(community.keys())
+names = [community[key]['name'] for key in keys]
+types = [character[agents.index(name)] for name in names]
+print(types)
+print(len(types), types.count('H'), types.count('C'), types.count('S'))
